195edf57a2f16835387444afe58035368e09e111
Francisco Costa <A48282@alunos.isel.pt>
2023-03-13 21:39:39 +0000

    Now using flake8 as linter and pytest for tests

--- rtsp/constants.py
+++
@@ -1,3 +0,0 @@
-""" Constants for the rtsp package. """
-
-RTSP_URL_EXAMPLE = 'rtsp://example.com:554/axis-media/media.amp'
---
+++ tests/version_test.py
@@ -0,0 +1,5 @@
+from src.image_processor import __version__
+
+
+def test_version():
+    assert __version__ == "0.1.0"
