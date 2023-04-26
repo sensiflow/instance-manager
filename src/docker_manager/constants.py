634bc1bc67553f8aee30fc9182dec46e2b244430
Francisco Costa <A48282@alunos.isel.pt>
2023-04-26 16:05:19 +0100

    changed dockerfiles directory to '/' instead of '\'

--- src/docker_manager/constants.py
+++ src/docker_manager/constants.py
@@ -1,4 +1,4 @@
-DOCKERFILE_CPU = "docker\\CPU"
-DOCKERFILE_GPU = "docker\\GPU"
+DOCKERFILE_CPU = "docker/CPU"
+DOCKERFILE_GPU = "docker/GPU"
 TAG_CPU = "image-processor-cpu"
 TAG_GPU = "image-processor-gpu"
